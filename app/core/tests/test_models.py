8de9c360250aea1097b307df04ea984b2fd98c11
Bartosz Ligęza <bligeza@tidk.pl>
2019-05-16 11:27:52 +0200

    test if email is normalized and if user provided an email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,14 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    # test the email for a new user is normalized
+    def test_new_user_email_normalized(self):
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+
+    # test creating user with no email raises error
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
