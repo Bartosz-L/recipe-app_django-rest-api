175974abdf9120b065cb92a0c3eed8bd00e9f40e
Bartosz Ligęza <bligeza@tidk.pl>
2019-05-16 14:08:12 +0200

    tests for admin page where u can create new user

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -25,9 +25,16 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    # test that  user edit page works
+    # test that user edit page works
     def test_user_page_change(self):
         url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    # test that the create user page works
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
