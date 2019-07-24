21bbb6c564fe2ed534e92db49a985de3429c5300
Bartosz Ligęza <bligeza@tidk.pl>
2019-07-24 09:56:05 +0200

    created view for creating user and added urls

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework.generics import CreateAPIView
+from user.serializers import UserSerializer
 # Create your views here.
+
+
+class CreateUserView(CreateAPIView):
+    # create a new user in the system
+    serializer_class = UserSerializer
