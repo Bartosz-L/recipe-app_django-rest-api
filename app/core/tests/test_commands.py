9644718aa88c3454aeb53cf68c709188a810a516
Bartosz Ligęza <bligeza@tidk.pl>
2019-07-24 10:58:17 +0200

    auth token view and serializer

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,9 +3,10 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
 
+
 class CommandTest(TestCase):
     def test_wait_for_db_ready(self):
-        #test waiting for db when db is available
+        # test waiting for db when db is available
         with patch('django.db.utils.ConnectionHandler.__getitem__') as getitem:
             getitem.return_value = True
             call_command('wait_for_db')
