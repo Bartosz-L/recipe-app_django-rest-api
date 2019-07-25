0aa6bdbeef6624f73a4830a7b3d0626e6c4ee031
Bartosz Ligęza <bligeza@tidk.pl>
2019-07-25 12:07:38 +0200

    added serializers, urls and view for updating the user

--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,12 @@
-from rest_framework.generics import CreateAPIView
+
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer, AuthTokenSerializer
 # Create your views here.
 
 
-class CreateUserView(CreateAPIView):
+class CreateUserView(generics.CreateAPIView):
     # create a new user in the system
     serializer_class = UserSerializer
 
@@ -14,3 +15,14 @@ class CreateTokenView(ObtainAuthToken):
     # create a new auth token for user
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    # manage the authenticated user
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        # retrieve and return authentication user
+        return self.request.user
