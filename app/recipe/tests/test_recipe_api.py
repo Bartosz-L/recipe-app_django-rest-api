3b3b99de15daf8425b1bd0b1beab11ac8e8c2c9b
Bartosz Ligęza <bligeza@tidk.pl>
2019-07-29 10:21:50 +0200

    fixed test, added functionality to upload images

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -15,7 +15,7 @@ RECIPES_URL = reverse('recipe:recipe-list')
 
 def image_upload_url(recipe_id):
     # return URL for recipe image upload
-    return reverse('recipe:recipe-upload-image, args=[recipe_id]')
+    return reverse('recipe:recipe-upload-image', args=[recipe_id])
 
 
 def detail_url(recipe_id):
